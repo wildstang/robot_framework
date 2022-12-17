d8e1f879d256ff6ac5c831ef5cde263473be6984
Liam Fruzyna <liam@fruzyna.net>
2022-12-17 08:47:50 -0600

    Updated solenoids to have second constructors for specifying PCM CAN ID

--- src/main/java/org/wildstang/hardware/roborio/outputs/WsDoubleSolenoid.java
+++ src/main/java/org/wildstang/hardware/roborio/outputs/WsDoubleSolenoid.java
@@ -15,7 +15,7 @@ public class WsDoubleSolenoid extends DiscreteOutput {
     /**
      * Constructs the solenoid from config.
      * @param name Descriptive name of the solenoid.
-     * @param module CAN id of the PCM the solenoid is connected to.
+     * @param module Model of PCM used which the solenoid is connected to.
      * @param channel1 Hardware port number the first solenoid is connected to.
      * @param channel2 Hardware port number the second solenoid is connected to.
      * @param p_default Default state.
@@ -24,6 +24,22 @@ public class WsDoubleSolenoid extends DiscreteOutput {
         super(name, p_default.ordinal());
 
         solenoid = new DoubleSolenoid(module, channel1, channel2);
+    }
+
+    /**
+     * Constructs the solenoid from config.
+     * This constructor is used if the CAN ID of the PCM is not the default value.
+     * @param name Descriptive name of the solenoid.
+     * @param canId CAN id of the PCM the solenoid is connected to.
+     * @param module Model of PCM used which the solenoid is connected to.
+     * @param channel1 Hardware port number the first solenoid is connected to.
+     * @param channel2 Hardware port number the second solenoid is connected to.
+     * @param p_default Default state.
+     */
+    public WsDoubleSolenoid(String name, int canId, PneumaticsModuleType module, int channel1, int channel2, WsDoubleSolenoidState p_default) {
+        super(name, p_default.ordinal());
+
+        solenoid = new DoubleSolenoid(canId, module, channel1, channel2);
     }
 
     /**
