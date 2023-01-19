acbffc443fe05b9eae16c2ea261f0d1ac7b2c4ca
Liam Fruzyna <liam@fruzyna.net>
2023-01-18 18:00:31 -0600

    Updated swerve path follower to use pathplanner

--- src/main/java/org/wildstang/framework/auto/steps/SwervePathFollowerStep.java
+++ src/main/java/org/wildstang/framework/auto/steps/SwervePathFollowerStep.java
@@ -3,12 +3,15 @@ package org.wildstang.framework.auto.steps;
 import org.wildstang.framework.auto.AutoStep;
 import org.wildstang.framework.subsystems.swerve.SwerveDriveTemplate;
 
+import com.pathplanner.lib.PathPlannerTrajectory;
+
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
 public class SwervePathFollowerStep extends AutoStep {
 
     private static final double ftToIn = 12;
+    private static final double mToIn = 30.3701;
     private static final int positionP = 7;
     //private static final int velocityP = 8;
     private static final int headingP = 15;
@@ -16,10 +19,7 @@ public class SwervePathFollowerStep extends AutoStep {
     //0   1  2    3          4       5        6          7          8         9          10        11         12       13        14       15
 
     private SwerveDriveTemplate m_drive;
-    private double[][] pathData;
-
-    private int counter;
-    private double lastPos;
+    private PathPlannerTrajectory pathData;
 
     private Timer timer;
 
@@ -28,7 +28,7 @@ public class SwervePathFollowerStep extends AutoStep {
      * @param pathData double[][] that contains path, should be from \frc\paths
      * @param drive the swerveDrive subsystem
      */
-    public SwervePathFollowerStep(double[][] pathData, SwerveDriveTemplate drive) {
+    public SwervePathFollowerStep(PathPlannerTrajectory pathData, SwerveDriveTemplate drive) {
         this.pathData = pathData;
         m_drive = drive;
         timer = new Timer();
@@ -37,8 +37,6 @@ public class SwervePathFollowerStep extends AutoStep {
     @Override
     public void initialize() {
         //start path
-        counter = 0;
-        lastPos = 0;
         m_drive.resetDriveEncoders();
         m_drive.setToAuto();
         timer.start();
@@ -46,25 +44,14 @@ public class SwervePathFollowerStep extends AutoStep {
 
     @Override
     public void update() {
-        if (counter >= pathData.length) {
+        if (timer.get() >= pathData.getTotalTimeSeconds()) {
+            m_drive.setAutoValues(0,0, -pathData.getEndState().poseMeters.getRotation().getDegrees());
             setFinished();
         } else {
-            SmartDashboard.putNumber("Auto Time", counter*0.02);
-            SmartDashboard.putNumber("Auto Attempted position", pathData[counter][positionP]);
-            if (lastPos == pathData[counter][positionP]){
-                //end path
-                m_drive.setAutoValues(pathData[counter][positionP]*ftToIn, 0, -Math.toDegrees(pathData[counter][headingP]));
-                counter = pathData.length;
-            } else if (timer.get()>=0.02){
-                //update values the robot is tracking to
-                System.out.println("Target position: " + pathData[counter][positionP]*ftToIn);
-                m_drive.setAutoValues(pathData[counter][positionP]*ftToIn, ((pathData[counter][positionP] - lastPos)/0.02)*ftToIn, -Math.toDegrees(pathData[counter][headingP]));
-                lastPos = pathData[counter][positionP];
-                counter++;
-                timer.advanceIfElapsed(0.02);
+            SmartDashboard.putNumber("Auto Time", timer.get());
+            //update values the robot is tracking to
+            m_drive.setAutoValues(0, pathData.sample(timer.get()).velocityMetersPerSecond * mToIn, -pathData.sample(timer.get()).poseMeters.getRotation().getDegrees());
             }
-        }
-        //System.out.println("Time: " + timer.get() + ", counter " + (counter-1));
     }
 
     @Override
